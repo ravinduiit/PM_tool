fbdd16515a315bb1cfce78e59b4f0fc9679ed57d
Pesala Sasindu <141388438+SasiSrinayaka@users.noreply.github.com>
2024-12-01 11:06:54 +0530

    changed the navbar css

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-        
-
         <title>@yield('title', 'Default Title')</title>
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -15,23 +13,22 @@
 
     </head>
     <body>
-        <div class="container-fluid d-flex">
-            <!-- Sidebar -->
-            <header>
-                @include('layout.navbar')
-            </header>
-
-            <!-- Main Content -->
-            <main>
-                @yield('content')
-            </main>
+    <div class="container-fluid d-flex">
+        <!-- Sidebar -->
+        <div id="sidebar" class="col-md-1 col-lg-1 p-1 d-md-block rounded-3 sidebar collapse">
+            @include('layout.navbar')
         </div>
 
-        <!-- Footer -->
-        @include('layout.footer')
+        <!-- Main Content -->
+        <main class="col-md-11 col-lg-11 p-4">
+            @yield('content')
+        </main>
+    </div>
 
-        @yield('script')
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Footer -->
+    @include('layout.footer')
 
-    </body>
+    @yield('script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
